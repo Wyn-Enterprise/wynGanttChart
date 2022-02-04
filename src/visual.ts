00b35556219413c639b5b8e527ba18087d2cac79
Abdias Michael <abdias.michael@grapecity.com>
2022-02-05 01:12:34 +0530

    Added new properties to handle null end date values

--- src/visual.ts
+++ src/visual.ts
@@ -1,5 +1,5 @@
 import '../style/visual.less';
-import {gantt} from 'dhtmlx-gantt';
+import { gantt } from 'dhtmlx-gantt';
 
 export default class Visual extends WynVisual {
     private container: HTMLDivElement;
@@ -15,13 +15,14 @@ export default class Visual extends WynVisual {
     private configGantt() {
         gantt.config.readonly = true;
         gantt.config.date_format = "%Y-%m-%d %H:%i";
+        gantt.config.show_unscheduled = true;
         gantt.i18n.setLocale("en");
         gantt.config.scale_unit = "week";
         gantt.config.date_scale = "%Y";
         //gantt.config.scale_unit = "month";
         gantt.config.scale_height = 50;
         gantt.config.min_column_width = this.properties.dateColumnWidth;
-        gantt.config.select_task  = false;
+        gantt.config.select_task = false;
         gantt.config.subscales = [
             {
                 unit: 'day',
@@ -29,11 +30,11 @@ export default class Visual extends WynVisual {
                 date: '%m-%d',
             }
         ];
-        gantt.config.columns=[
+        gantt.config.columns = [
             {
-                name:"text",
+                name: "text",
                 label: this.properties.label,
-                tree:true,
+                tree: true,
                 width: this.properties.projectWidth
             }
         ];
@@ -50,21 +51,22 @@ export default class Visual extends WynVisual {
         this.className = this.properties.style;
         gantt.init(this.container.id);
         this.isMock = true;
+        let self = this;
         let ganttData = [];
         let statusArr = [];
         let mockGanttData = [
-            { id:1, text: "Project1", color:'#FF8C00'},
-            { parent:1, text: "Requesting Research", start_date: "2019-07-25 00:00", end_date: "2019-07-31 00:00", progress:0.6, state:'finished', color:'#FFD700'},
-            { parent:1, text: "Product Design", start_date: "2019-07-28 00:00", end_date: "2019-07-30 00:00", progress:0.6, state:'unfinished', color: '#FF8C00'},
-            { parent:1, text: "Development", start_date: "2019-08-01 15:00:00",  end_date: "2019-08-05 00: 00", state:'canceled', color: '#FF0000'},
-            { id:2, text: "Project2", color:'#FF8C00'},
-            { parent: 2, text: "Requesting Research", start_date: "2019-07-20 00:00", end_date: "2019-07-25 00:00", state:'finished', color:'#FFD700'},
-            { parent:2, text: "Product Design", start_date: "2019-07-25 00:00", end_date: "2019-07-30 00:00", state:'unfinished', color: '#FF8C00'}
+            { id: 1, text: "Project1", color: '#FF8C00' },
+            { parent: 1, text: "Requesting Research", start_date: "2019-07-25 00:00", end_date: "2019-07-31 00:00", progress: 0.6, state: 'finished', color: '#FFD700' },
+            { parent: 1, text: "Product Design", start_date: "2019-07-28 00:00", end_date: "2019-07-30 00:00", progress: 0.6, state: 'unfinished', color: '#FF8C00' },
+            { parent: 1, text: "Development", start_date: "2019-08-01 15:00:00", end_date: "2019-08-05 00: 00", state: 'canceled', color: '#FF0000' },
+            { id: 2, text: "Project2", color: '#FF8C00' },
+            { parent: 2, text: "Requesting Research", start_date: "2019-07-20 00:00", end_date: "2019-07-25 00:00", state: 'finished', color: '#FFD700' },
+            { parent: 2, text: "Product Design", start_date: "2019-07-25 00:00", end_date: "2019-07-30 00:00", state: 'unfinished', color: '#FF8C00' }
         ];
         if (options.dataViews[0]) {
             let profile = options.dataViews[0].plain.profile;
             let profileName = {};
-            for(let item in profile) {
+            for (let item in profile) {
                 if (profile[item].values.length) {
                     profileName[item] = profile[item].values[0].display;
                 }
@@ -82,7 +84,7 @@ export default class Visual extends WynVisual {
             let projectObj = {};
             dataArray.forEach((data) => {
                 let reg = /\d{4}\-\d{2}\-\d{2}T\d{2}\:\d{2}\:\d{2}/;
-                if(!reg.test(data[profileName['startTime']].toString()) || !reg.test(data[profileName['endTime']].toString())){
+                if ((data[profileName['startTime']] != null && !reg.test(data[profileName['startTime']].toString())) || (data[profileName['endTime']] != null && !reg.test(data[profileName['endTime']].toString()))) {
                     ganttData = mockGanttData;
                     return;
                 }
@@ -102,8 +104,32 @@ export default class Visual extends WynVisual {
                 projectObj[projectIndex] = [];
                 bindProjectObj[projectIndex].forEach((item) => {
                     function convertDate(date) {
-                        let dateString = item[profileName[date]].toString();
-                        return  dateString.replace(/T/g, ' ');
+                        let dateString = item[profileName[date]];
+                        if (dateString == null) {
+                            switch (self.properties.nullEndDate) {
+                                case "startDate":
+                                    dateString = item[profileName["startTime"]].toString();
+                                    break;
+                                case "today":
+                                    dateString = new Date().toISOString();
+                                    break;
+                                case "futureDate":
+                                    let daysInFuture = self.properties.daysInFuture;
+                                    let tempDate;
+                                    if (self.properties.relativeTo == "relativeToStartDate") {
+                                        tempDate = new Date(item[profileName["startTime"]]);
+                                    }
+                                    else if (self.properties.relativeTo == "relativeToToday") {
+                                        tempDate = new Date();
+                                    }
+                                    tempDate.setDate(tempDate.getDate() + daysInFuture);
+                                    dateString = tempDate.toISOString();
+                                    break;
+                            }
+                        }
+                        else
+                            dateString = item[profileName[date]].toString();
+                        return dateString.replace(/T/g, ' ');
                     }
 
                     function getColor(status) {
@@ -128,7 +154,7 @@ export default class Visual extends WynVisual {
                     }
 
                     if (statusArr) {
-                        task['status'] =  item[profileName['status']];
+                        task['status'] = item[profileName['status']];
                         task['color'] = getColor(item[profileName['status']])
                     }
 
@@ -136,7 +162,7 @@ export default class Visual extends WynVisual {
                 })
                 if (profileName['projectName']) {
                     let parentItem = {
-                        id : parentId,
+                        id: parentId,
                         text: projectIndex,
                         color: this.properties.projectColor
                     }
@@ -169,9 +195,10 @@ export default class Visual extends WynVisual {
     }
 
     public getInspectorHiddenState(options: VisualNS.IVisualUpdateOptions): string[] {
-        if (options.dataViews[0] && !options.dataViews[0].plain.profile.status.values.length) {
-            return ['taskColors'];
+        if (options.properties.nullEndDate === 'startDate' || options.properties.nullEndDate === 'today') {            
+            return ['relativeTo', 'daysInFuture'];
         }
+        return null;
     }
 
     public getActionBarHiddenState(options: VisualNS.IVisualUpdateOptions): string[] {
